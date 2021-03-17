7780a6108b2d580532c22935fdd9bea56cecc1a3
Dawit <dawitta10@gmail.com>
2021-03-17 04:29:22 -0400

    second commit

---
+++ src/main/java/VariousConcepts/TestGit.java
@@ -0,0 +1,12 @@
+package VariousConcepts;
+
+public class TestGit {
+ public static void main(String[] args) {
+	 printMe();
+	
+}
+ public static void printMe() {
+	 
+	 System.out.println("test git");
+ }
+}
