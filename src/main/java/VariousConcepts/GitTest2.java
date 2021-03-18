7c92aa39dab88948fec28ad3e70baff81694e55e
Dawit <dawitta10@gmail.com>
2021-03-18 00:42:30 -0400

    third commit

---
+++ src/main/java/VariousConcepts/GitTest2.java
@@ -0,0 +1,25 @@
+package VariousConcepts;
+
+public class GitTest2 {
+	public static void main(String[] args) {
+		 printMe();
+		 printMe1();
+		 printMe2();
+		 
+	}
+	 public static void printMe() {
+		 
+		 System.out.println("test git");
+	 }
+	 
+	 public static void printMe1() {
+		 
+		 System.out.println("test git 1 ");
+	 }
+	 
+ public static void printMe2() {
+		 
+		 System.out.println("test git 1 ");
+	}
+
+
